b5d8eb8e7de64577eb9c8e7640cfcbe2c0bb3bf2
molotkova <molotkova548@gmail.com>
2022-09-22 10:38:12 +0300

    changed data loading process

--- salary-prediction/stage1/stage1.py
+++ salary-prediction/stage1/stage1.py
@@ -1,11 +1,23 @@
+import os
+import requests
+
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_percentage_error as mape
 
+# checking ../Data directory presence
+if not os.path.exists('../Data'):
+    os.mkdir('../Data')
 
-# Load data
-data = pd.read_csv('data.csv')
+# download data if it is unavailable
+if 'data.csv' not in os.listdir('../Data'):
+    url = "https://www.dropbox.com/s/3cml50uv7zm46ly/data.csv?dl=1"
+    r = requests.get(url, allow_redirects=True)
+    open('../Data/fashion-mnist_train.csv', 'wb').write(r.content)
 
-# Your code here
-...
+# read data
+data = pd.read_csv('../Data/data.csv')
+
+# write your code here
+
